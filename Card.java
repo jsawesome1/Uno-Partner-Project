88a528600b9f7577ef2c984be907f01b4c223502
Brandthacker12 <64920649+Brandthacker12@users.noreply.github.com>
2020-05-18 16:27:50 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -10,8 +10,10 @@ public class Card {
 	double size;
 	Color cardColor;
 	int value;
+	int colVal;
 	boolean special;
 	public Card(int number, int color) { //1-9 color numbers and +2s are the value of 0
+		colVal = color;
 		if(color == 0)
     		cardColor = Color.BLUE;
     	if(color == 1)
@@ -21,7 +23,7 @@ public class Card {
     	if(color == 3)
     		cardColor = new Color(230,230,0);
     	special = false;
-    	if(number == 0)
+    	if(number == 10)
     		special = true;
     	value = number;
 	}
@@ -29,8 +31,16 @@ public class Card {
 		value = number;
 		special = true;
 	}
-	public void setCords(int x, int y) {
+	public int cardNum() {
+		return value;
+	}
+	public int cardCol() { 
+		return colVal;
+	}
+	public void setCords(int x, int y, double z) {
 		xPos = x;
 		yPos = y;
+		size = z;
 	}
 }
+
