afd081cd6f4dc79f36ae7613eb538fa4ac7ddc90
Brandthacker12 <64920649+Brandthacker12@users.noreply.github.com>
2020-05-18 16:27:30 -0400

    Update CardAni.java

--- CardAni.java
+++ CardAni.java
@@ -1,27 +1,30 @@
 import java.awt.Color;
-class cardAni {
+import java.awt.Font;
+import java.awt.Graphics;
+
+class CardAni {
 	Color cardColor;
 	int xPos, yPos;
 	double size;
 	int value;
 	String display;
 	Boolean special;
-	public cardAni(Card base, Graphics g) { // card class already made
+	public CardAni(Card base, Graphics g) { // card class already made
 		xPos = base.xPos;
 		yPos = base.yPos;
+		size = base.size;
 		value = base.value;
 		cardColor = base.cardColor;
-		if(value==0)
+		if(value==10)
     		display = "+2";
     	else
     		display = ""+value+"";
 		special = false;
-		if(value == 0)
+		if(value == 10)
 			special = true;
-		size = 1;
     	paintCard(g);
     }
-    public cardAni(int num, int color, int x, int y, double scale, Graphics g) { //no actually card class made or being passed
+    public CardAni(int num, int color, int x, int y, double scale, Graphics g) { //no actually card class made or being passed
     	if(color == 0)
     		cardColor = Color.BLUE;
     	if(color == 1)
@@ -29,7 +32,7 @@ class cardAni {
     	if(color == 2)
     		cardColor = Color.GREEN;
     	if(color == 3)
-    		cardColor = Color.YELLOW;
+    		cardColor = new Color(230,230,0);
     	xPos = x;
     	yPos = y;
     	size = scale;
@@ -37,7 +40,7 @@ class cardAni {
     	special = false;
 		if(value == 0)
 			special = true;
-    	if(num==0)
+    	if(num==10)
     		display = "+2";
     	else
     		display = ""+num+"";
@@ -86,4 +89,9 @@ class cardAni {
        	g.drawRect(xPos+(int)(25*size), yPos+(int)(375*size), (int)(200*size), (int)(25*size));
        	
     }
+    public void setCords(int x, int y, Graphics g) {
+		xPos = x;
+		yPos = y;
+		paintCard(g);
+	}
 }
